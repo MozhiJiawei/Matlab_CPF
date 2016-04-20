fade7397be99973866c7d8598d4726569b5a6488
Mozhi Jiawei <qetwe0000@gmail.com>
2016-04-20 17:41:47 +0800

    case 118

--- example.m
+++ example.m
@@ -1,9 +1,11 @@
 define_constants;
+% setup parameters
+% mpopt is a parameter struct
 mpopt = mpoption('out.all', 0, 'verbose', 2);
-mpopt = mpoption(mpopt, 'cpf.stop_at', 'FULL', 'cpf.step', 0.2);
-mpopt = mpoption(mpopt, 'cpf.plot.level', 2);
+mpopt = mpoption(mpopt, 'cpf.stop_at', 'FULL', 'cpf.adapt_step', 1);
+mpopt = mpoption(mpopt, 'cpf.plot.level', 2);  
 
-mpcb = loadcase(t_case9_pfv2); % load base case
+mpcb = loadcase(case118); % load base case
 mpct = mpcb; % set up target case with
 mpct.gen(:, [PG QG]) = mpcb.gen(:, [PG QG]) * 2.5; % increased generation
 mpct.bus(:, [PD QD]) = mpcb.bus(:, [PD QD]) * 2.5; % and increased load
